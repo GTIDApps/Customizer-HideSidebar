f3f164362cc4ebd74e7d32d76c836fd22c9a2ece
Jasmine <charlotte.subroto@id.gt.com>
2025-06-20 13:35:56 +0700

    checked if pushed

---
+++ src/extensions/customizerHidesidebar/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICustomizerHidesidebarApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CustomizerHidesidebarApplicationCustomizerStrings' {
+  const strings: ICustomizerHidesidebarApplicationCustomizerStrings;
+  export = strings;
+}
